3162d03bc876851188f14537c20325ef355138f4
bravo-kernel <bravo-kernel@users.noreply.github.com>
2015-07-23 12:55:58 +0000

    Fixes file storage exception

--- tests/TestCase/Routing/Filter/ThrottleFilterTest.php
+++ tests/TestCase/Routing/Filter/ThrottleFilterTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace Muffin\Throttle\Test\TestCase\Routing\Filter;
 
+use Cake\Cache\Cache;
 use Cake\Event\Event;
+use Cake\Network\Request;
 use Cake\TestSuite\TestCase;
 use Muffin\Throttle\Routing\Filter\ThrottleFilter;
 
@@ -29,4 +31,22 @@ class ThrottleFilterTest extends TestCase
         $this->assertEquals(429, $result->statusCode());
     }
 
+    /**
+     * Using the File Storage cache engine should throw a LogicException.
+     *
+     * @expectedException \LogicException
+     */
+    public function testFileCacheException()
+    {
+        Cache::config('file', [
+            'className' => 'Cake\Cache\Engine\FileEngine',
+            'prefix' => 'throttle_'
+        ]);
+
+        $object = new ThrottleFilter();
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod('_touch');
+        $method->setAccessible(true);
+        $method->invokeArgs($object, [new Request()]);
+    }
 }
