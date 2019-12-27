861c1850b59d1f6c3b2fb8b2edcb5d96528a9ee8
ADmad <admad.coder@gmail.com>
2019-12-27 23:05:52 +0530

    Clean up code

--- src/Middleware/ThrottleMiddleware.php
+++ src/Middleware/ThrottleMiddleware.php
@@ -3,23 +3,65 @@ declare(strict_types=1);
 
 namespace Muffin\Throttle\Middleware;
 
+use Cake\Cache\Cache;
 use Cake\Core\InstanceConfigTrait;
 use Cake\Http\Response;
-use Cake\Http\ServerRequest;
-use Muffin\Throttle\ThrottleTrait;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class ThrottleMiddleware
+class ThrottleMiddleware implements MiddlewareInterface
 {
     use InstanceConfigTrait;
-    use ThrottleTrait;
 
     /**
-     * Default Configuration array
+     * Default config.
      *
      * @var array
      */
-    protected $_defaultConfig = [];
+    protected $_defaultConfig = [
+        'response' => [
+            'body' => 'Rate limit exceeded',
+            'type' => 'text/html',
+            'headers' => [],
+        ],
+        'interval' => '+1 minute',
+        'limit' => 10,
+        'headers' => [
+            'limit' => 'X-RateLimit-Limit',
+            'remaining' => 'X-RateLimit-Remaining',
+            'reset' => 'X-RateLimit-Reset',
+        ],
+    ];
+
+    /**
+     * Cache configuration name
+     *
+     * @var string
+     */
+    public static $cacheConfig = 'throttle';
+
+    /**
+     * Cache expiration key suffix
+     *
+     * @var string
+     */
+    public static $cacheExpirationSuffix = 'expires';
+
+    /**
+     * Unique client identifier
+     *
+     * @var string
+     */
+    protected $_identifier;
+
+    /**
+     * Number of connections after increment
+     *
+     * @var int
+     */
+    protected $_count;
 
     /**
      * ThrottleMiddleware constructor.
@@ -28,19 +70,21 @@ class ThrottleMiddleware
      */
     public function __construct(array $config = [])
     {
-        $config = array_replace_recursive($this->_setConfiguration(), $config);
+        $config += ['identifier' => function ($request) {
+            return $request->clientIp();
+        }];
+
         $this->setConfig($config);
     }
 
     /**
      * Called when the class is used as a function
      *
-     * @param \Cake\Http\ServerRequest $request Request object
-     * @param \Cake\Http\Response $response Response object
-     * @param callable $next Next class in middleware
-     * @return \Psr\Http\Message\ResponseInterface
+     * @param \Psr\Http\Message\ServerRequestInterface $request The request.
+     * @param \Psr\Http\Server\RequestHandlerInterface $handler The request handler.
+     * @return \Psr\Http\Message\ResponseInterface A response.
      */
-    public function __invoke(ServerRequest $request, Response $response, callable $next): ResponseInterface
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $this->_setIdentifier($request);
         $this->_initCache();
@@ -49,6 +93,8 @@ class ThrottleMiddleware
         $config = $this->getConfig();
 
         if ($this->_count > $config['limit']) {
+            $response = new Response();
+
             if (is_array($config['response']['headers'])) {
                 foreach ($config['response']['headers'] as $name => $value) {
                     $response = $response->withHeader($name, $value);
@@ -61,13 +107,140 @@ class ThrottleMiddleware
                 $message = $config['response']['body'];
             }
 
-            return $response->withStatus(429)
+            return $response
+                ->withStatus(429)
                 ->withType($config['response']['type'])
                 ->withStringBody($message);
         }
 
-        $response = $next($request, $response);
+        $response = $handler->handle($request);
 
         return $this->_setHeaders($response);
+    }
+
+    /**
+     * Sets the identifier class property. Uses Throttle default IP address
+     * based identifier unless a callable alternative is passed.
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request RequestInterface instance
+     * @return void
+     * @throws \InvalidArgumentException
+     */
+    protected function _setIdentifier(ServerRequestInterface $request): void
+    {
+        $key = $this->getConfig('identifier');
+        if (!is_callable($this->getConfig('identifier'))) {
+            throw new \InvalidArgumentException('Throttle identifier option must be a callable');
+        }
+        $this->_identifier = $key($request);
+    }
+
+    /**
+     * Initializes cache configuration.
+     *
+     * @return void
+     */
+    protected function _initCache(): void
+    {
+        if (Cache::getConfig(static::$cacheConfig) === null) {
+            Cache::setConfig(static::$cacheConfig, [
+                'className' => $this->_getDefaultCacheConfigClassName(),
+                'prefix' => static::$cacheConfig . '_' . $this->_identifier,
+                'duration' => $this->getConfig('interval'),
+            ]);
+        }
+    }
+
+    /**
+     * Gets the className of the default CacheEngine so the Throttle cache
+     * config can use the same. String cast is required to catch a DebugEngine
+     * array/object for users with DebugKit enabled.
+     *
+     * @return string ClassName property of default Cache engine
+     */
+    protected function _getDefaultCacheConfigClassName(): string
+    {
+        $config = Cache::getConfig('default');
+        $engine = (string)$config['className'];
+
+        // short cache engine names can be returned immediately
+        if (strpos($engine, '\\') === false) {
+            return $engine;
+        }
+        // fully namespace cache engine names need extracting class name
+        preg_match('/.+\\\\(.+)Engine/', $engine, $matches);
+
+        return $matches[1];
+    }
+
+    /**
+     * Atomically updates cache using default CakePHP increment offset 1.
+     *
+     * Please note that the cache key needs to be initialized to prevent
+     * increment() failing on 0. A separate cache key is created to store
+     * the interval expiration time in epoch.
+     *
+     * @return int
+     */
+    protected function _touch(): int
+    {
+        if (Cache::read($this->_identifier, static::$cacheConfig) === null) {
+            Cache::write($this->_identifier, 0, static::$cacheConfig);
+            Cache::write(
+                $this->_getCacheExpirationKey(),
+                strtotime($this->getConfig('interval'), time()),
+                static::$cacheConfig
+            );
+        }
+
+        return Cache::increment($this->_identifier, 1, static::$cacheConfig) ?? 0;
+    }
+
+    /**
+     * Returns cache key holding the epoch cache expiration timestamp.
+     *
+     * @return string Cache key holding cache expiration in epoch.
+     */
+    protected function _getCacheExpirationKey(): string
+    {
+        return $this->_identifier . '_' . static::$cacheExpirationSuffix;
+    }
+
+    /**
+     * Extends response with X-headers containing rate limiting information.
+     *
+     * @param \Psr\Http\Message\ResponseInterface $response ResponseInterface instance
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    protected function _setHeaders(ResponseInterface $response): ResponseInterface
+    {
+        $headers = $this->getConfig('headers');
+
+        if (!is_array($headers)) {
+            return $response;
+        }
+
+        return $response
+            ->withHeader($headers['limit'], (string)$this->getConfig('limit'))
+            ->withHeader($headers['remaining'], (string)$this->_getRemainingConnections())
+            ->withHeader(
+                $headers['reset'],
+                (string)Cache::read($this->_getCacheExpirationKey(), static::$cacheConfig)
+            );
+    }
+
+    /**
+     * Calculates the number of hits remaining before client reaches rate limit.
+     *
+     * @return int Number of remaining client hits, zero if limit is reached
+     */
+    protected function _getRemainingConnections(): int
+    {
+        $remaining = $this->getConfig('limit') - $this->_count;
+        if ($remaining <= 0) {
+            return 0;
+        }
+
+        return $remaining;
     }
 }
--- src/Plugin.php
+++ src/Plugin.php
@@ -1,25 +1,16 @@
 <?php
 declare(strict_types=1);
 
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         1.0.2
- * @license       https://www.opensource.org/licenses/mit-license.php MIT License
- */
 namespace Muffin\Throttle;
 
 use Cake\Core\BasePlugin;
 
-/**
- * Plugin class for CakePHP 3.6.0 plugin collection.
- */
 class Plugin extends BasePlugin
 {
+    /**
+     * The name of this plugin
+     *
+     * @var string
+     */
+    protected $name = 'Throttle';
 }
