c0365480a111f7a3e8ebfc3f7decd0c19e7331b3
Adam Rusinowski <adam.rusinowski@gmail.com>
2023-09-05 07:46:52 +0200

    restored simple test bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+use Cake\Core\Plugin;
+use Muffin\Throttle\ThrottlePlugin;
+
 /**
  * Test suite bootstrap.
  *
@@ -8,113 +11,19 @@ declare(strict_types=1);
  * has been installed as a dependency of the plugin, or the plugin is itself
  * installed as a dependency of an application.
  */
-
-use Cake\Cache\Cache;
-use Cake\Core\Configure;
-use Cake\Core\Plugin;
-use Cake\Datasource\ConnectionManager;
-use Cake\Log\Log;
-use Cake\TestSuite\Fixture\SchemaLoader;
-use Muffin\Throttle\ThrottlePlugin;
-
-require_once 'vendor/autoload.php';
-
-// Path constants to a few helpful things.
-if (!defined('DS')) {
-    define('DS', DIRECTORY_SEPARATOR);
-}
-define('ROOT', dirname(__DIR__));
-define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
-define('CORE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
-define('CAKE', CORE_PATH . 'src' . DS);
-define('TESTS', ROOT . DS . 'tests');
-define('APP', ROOT . DS . 'tests' . DS . 'test_app' . DS);
-define('APP_DIR', 'test_app');
-define('WEBROOT_DIR', 'webroot');
-define('WWW_ROOT', APP . 'webroot' . DS);
-define('TMP', sys_get_temp_dir() . DS);
-define('CONFIG', APP . 'config' . DS);
-define('CACHE', TMP);
-define('LOGS', TMP);
-
-require_once CORE_PATH . 'config/bootstrap.php';
-require_once CAKE . 'Core/functions_global.php';
-require_once CAKE . 'Collection/functions_global.php';
-
-date_default_timezone_set('UTC');
-mb_internal_encoding('UTF-8');
-
-Configure::write('debug', true);
-Configure::write('App', [
-    'namespace' => 'Muffin\Throttle',
-    'encoding' => 'UTF-8',
-    'base' => false,
-    'baseUrl' => false,
-    'dir' => 'src',
-    'webroot' => 'webroot',
-    'www_root' => APP . 'webroot',
-    'fullBaseUrl' => 'http://localhost',
-    'imageBaseUrl' => 'img/',
-    'jsBaseUrl' => 'js/',
-    'cssBaseUrl' => 'css/',
-    'paths' => [
-        'plugins' => [APP . 'Plugin' . DS],
-        'templates' => [APP . 'templates' . DS],
-    ],
-]);
-Configure::write('Session', [
-    'defaults' => 'php',
-]);
-
-Cache::setConfig([
-    '_cake_core_' => [
-        'engine' => 'File',
-        'prefix' => 'cake_core_',
-        'serialize' => true,
-    ],
-    '_cake_model_' => [
-        'engine' => 'File',
-        'prefix' => 'cake_model_',
-        'serialize' => true,
-    ],
-    'default' => [
-        'engine' => 'File',
-        'prefix' => 'default_',
-        'serialize' => true,
-    ],
-]);
-
-// Ensure default test connection is defined
-if (!getenv('DB_URL')) {
-    putenv('DB_URL=sqlite://127.0.0.1/' . TMP . 'test.sqlite');
-}
-
-$config = [
-    'url' => getenv('DB_URL'),
-    'timezone' => 'UTC',
-];
-
-ConnectionManager::setConfig('test', $config);
-
-Log::setConfig([
-    'debug' => [
-        'engine' => 'Cake\Log\Engine\FileLog',
-        'path' => LOGS,
-        'levels' => ['notice', 'info', 'debug'],
-        'file' => 'debug',
-    ],
-    'error' => [
-        'engine' => 'Cake\Log\Engine\FileLog',
-        'path' => LOGS,
-        'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
-        'file' => 'error',
-    ],
-]);
+$findRoot = function ($root) {
+    do {
+        $lastRoot = $root;
+        $root = dirname($root);
+        if (is_dir($root . '/vendor/cakephp/cakephp')) {
+            return $root;
+        }
+    } while ($root !== $lastRoot);
+    throw new Exception('Cannot find the root of the application, unable to run tests');
+};
+$root = $findRoot(__FILE__);
+unset($findRoot);
+chdir($root);
+require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
 
 Plugin::getCollection()->add(new ThrottlePlugin());
-
-// Create test database schema
-if (env('FIXTURE_SCHEMA_METADATA')) {
-    $loader = new SchemaLoader();
-    $loader->loadInternalFile(env('FIXTURE_SCHEMA_METADATA'));
-}
